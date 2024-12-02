0e2db13a26ba7566da4b779bbd4b3bdf443430cd
Giorgia Savioli <savioligiorgia@icloud.com>
2024-12-02 15:35:43 +0100

    System out modificato in "sono un numero divisibile per 3"

--- FizzBuzz/src/main/FizzBuzz.java
+++ FizzBuzz/src/main/FizzBuzz.java
@@ -9,9 +9,9 @@ public class FizzBuzz {
 			if (numero % 3 == 0 && numero % 5 == 0) { // se il numero è divisibile per 3 e SOLO SE se è divisibile anche per 5
 				System.out.println("FizzBuzz");   //stampa Fizz Buzz
 			}else if (numero % 3 == 0) {   // se il numero è divisibile per 3
-				System.out.println("Fizz");  //stampa Fizz
+				System.out.println("Ciao sono un numero divisibile per 3");  //stampa Ciao sono un numero divisibile per 3
 			}else if (numero % 5 == 0) {  // se il numero è divisibile per 5
-				System.out.println("Ciao, sono un numero divisibile per 5");  //stampa Ciao, sono un numero divisibile per 5
+				System.out.println("Ciao, sono un numero divisibile per 5");  //stampa Buzz
 			}else {
 				System.out.println(numero);  //Altrimenti stampa il numero (in cifra)
 			}
